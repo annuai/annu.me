bb71e726103a678207c388112253102cf24cc92d
Annuai <annuai.annu@gmail.com>
2026-05-18 17:49:40 +0530

    Added Pomo

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'serve-pomo',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url === '/pomo') {
+            req.url = '/pomo.html';
+          }
+          next();
+        });
+      }
+    }
+  ],
 })
