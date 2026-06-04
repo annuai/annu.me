4084159bcb5ac7b613a3d6ec4ec907303aaf41a3
Annuai <annuai.annu@gmail.com>
2026-06-04 22:10:15 +0530

    Add post: this time i skipped the donut

---
+++ src/posts/this-time-i-skipped-the-donut.jsx
@@ -0,0 +1,142 @@
+export const metadata = {
+    title: "This time I skipped the donut",
+    excerpt:
+        "Years after my first Blender tutorial, I came back to the software with a different goal: not to make a donut, but to learn the fundamentals while creating renders for my own products.",
+    date: "2026-06-04",
+};
+
+export default function Post() {
+    return (
+        <>
+            <p>
+                Like many people, my first experience with Blender years ago
+                involved making the famous donut. I followed the tutorials,
+                modeled the donut, added the icing, rendered it, and moved on.
+                At the time, it felt like I had learned Blender.
+            </p>
+
+            <p>
+                Looking back, I had only learned how to complete a tutorial.
+            </p>
+
+            <p>
+                Recently, I decided to pick Blender up again. My goal wasn't to
+                become a 3D artist or learn every corner of the software. I
+                simply wanted to create better renders for the products I
+                design. I wanted to be able to communicate ideas more clearly
+                and present products in a way that felt closer to the vision in
+                my head.
+            </p>
+
+            <p>
+                This time, I deliberately skipped the donut.
+            </p>
+
+            <p>
+                Instead, I imported one of my own products into a downloaded
+                scene and tried to get a render out. It sounded simple enough.
+                It wasn't.
+            </p>
+
+            <p>
+                I quickly remembered why Blender had always felt intimidating.
+                The interface is unforgiving. There are shortcuts for
+                everything. Multiple ways to do the same thing. Settings hidden
+                inside menus that seem impossible to find when you need them.
+                Sometimes it feels like the software expects you to already know
+                how it works.
+            </p>
+
+            <p>
+                One minute I was trying to move an object and couldn't
+                understand why it was jumping across the scene. The next I was
+                wondering why plastic looked like metal. Then I was fighting
+                with cameras, textures, materials, lighting, and all the other
+                things that stand between a CAD model and a believable render.
+            </p>
+
+            <p>
+                The more time I spent with Blender, the more I realized that I
+                had never really understood its core concepts. I knew workflows,
+                not fundamentals. I knew how to follow instructions, but not
+                how to solve problems when something unexpected happened.
+            </p>
+
+            <p>
+                What made this experience different was that I wasn't relying on
+                tutorials. Instead of following a predefined path, I was solving
+                problems as they appeared.
+            </p>
+
+            <p>
+                Why is this texture not sitting correctly on the model?
+            </p>
+
+            <p>
+                Why does this material look unrealistic?
+            </p>
+
+            <p>
+                Why won't the camera move?
+            </p>
+
+            <p>
+                Why does this render still feel fake even though everything
+                seems technically correct?
+            </p>
+
+            <p>
+                ChatGPT ended up becoming a surprisingly useful learning
+                companion throughout the process. Whenever I got stuck, I could
+                ask a question and immediately continue experimenting. It felt
+                less like watching a tutorial and more like having someone sit
+                beside me while I worked through problems.
+            </p>
+
+            <p>
+                Instead of searching through forums or skipping through
+                forty-minute videos hoping the answer was hidden somewhere
+                inside, I could stay focused on the task in front of me.
+            </p>
+
+            <p>
+                By the end of the day, I had a render that I'm reasonably happy
+                with. More importantly, I understood a little more about why it
+                looked the way it did. I learned things that experienced Blender
+                users probably take for granted: materials, normals, texture
+                mapping, cameras, lighting, modifiers, and all the small
+                details that make a render feel believable.
+            </p>
+
+            <img
+                src="/posts/this-time-i-skipped-the-donut/hornbill-lamp.png"
+                alt="A render of the hornbill lamp created while learning Blender"
+            />
+
+            <p>
+                The render itself isn't the achievement.
+            </p>
+
+            <p>
+                The achievement is finally taking the time to learn the
+                fundamentals properly.
+            </p>
+
+            <p>
+                Over the next few weeks, I want to continue in the same way. Not
+                by chasing perfect renders, but by working on real products and
+                solving real problems. The renders will improve naturally. What
+                I'm really interested in is building a deeper understanding of
+                the tools and principles behind them.
+            </p>
+
+            <p>
+                Everyone starts with a donut.
+            </p>
+
+            <p>
+                This time, I started with something I actually wanted to make.
+            </p>
+        </>
+    );
+}
